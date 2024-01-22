5b2ae6b6483c141ee1e0b1a16c3ef865f2e75576
Rekman <rekman.heln@gmail.com>
2024-01-22 18:58:04 +0300

    Map tile class

---
+++ WaveFunctionCollapse/MapTile2D.cs
@@ -0,0 +1,24 @@
+﻿namespace WaveFunctionCollapse
+{
+    public class MapTile2D<T>
+    {
+        T[,] _data;
+        string[] _sidesHash = new string[4] { "a", "a", "a", "a" };
+
+        public MapTile2D(T[,] data)
+        {
+            _data = data;
+        }
+
+        public int Width { get => _data.GetLength(0); }
+        public int Height { get => _data.GetLength(1); }
+
+        public override int GetHashCode()
+        {
+            var res = _data.Length;
+            for(int i = 0; i < _data.Length; i++)
+                res *= _data[i % Width, i / Width].GetHashCode() ^ i;
+            return res;
+        }
+    }
+}
